79a1450eda4c30e88fbc2595bf9cf0e576e2c870
twinklestare <twinklelittle321@gmail.com>
2024-01-29 10:37:47 +0530

    [ADDED]:janvi - c program for leapyear

---
+++ leapyear.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int year;
+	
+	Printf ("\nEnter Any Number:");
+	scanf("%d",&year);
+	
+	if(year%4=0)
+	{
+		printf("\nLeap year :");
+	}
+	else
+	{
+		printf("\nNot Leap year:");
+	}
+	return 0;
+	
+}
